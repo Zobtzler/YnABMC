11accf781c1f3ac0bb1f9fc6f30a71b7d2d8a38d
Zobtzler <hanssonkarl@hotmail.se>
2019-09-15 00:01:56 +0200

    fixed version numbering

--- YnABMC/Form1.cs
+++ YnABMC/Form1.cs
@@ -21,8 +21,8 @@ namespace YnABMC
     public partial class Form1 : Form
     {
         
-        string SoftwareVersion = "Alpha 0.3.4";
-        //string SoftwareVersion = "Dev 0.3.4";
+        string SoftwareVersion = "Alpha 0.3.4.2";
+        //string SoftwareVersion = "Dev 0.3.4.2";
         string GameVersions = "1.2,2.0";
         string FolderPath = "", BmpFilePath = "", ProjectName = "", AuthorName = "", ModID = "";
         bool Lua = false;
